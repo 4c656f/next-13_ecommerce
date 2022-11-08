664ecbf268ea690eb659dde78d7697433a3e65e2
4c656f <vlevo.yovlyo@yandex.ru>
2022-11-08 22:55:58 +0600

    fix error

--- types/ITrpc.ts
+++
@@ -1,6 +0,0 @@
-import { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
-import { AppRouter } from "~/backend/routers/_app";
-
-
-export type Inputs = inferRouterInputs<AppRouter>;
-export type Outputs = inferRouterOutputs<AppRouter>;
